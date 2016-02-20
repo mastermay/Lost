3ad207af8980cb41618121982595827e89f1d726
mastermay <986492616@qq.com>
2016-02-20 13:18:31 +0800

    minify css & js

--- functions/theme.php
+++ functions/theme.php
@@ -10,12 +10,12 @@
 
 add_action( 'wp_enqueue_scripts', 'my_enqueue_scripts_frontpage' );
 function my_enqueue_scripts_frontpage() {
-	wp_enqueue_style( 'default', get_template_directory_uri() . '/style.css', array(), '1.1.0' );
-	wp_deregister_script( 'jquery' ); 
-	wp_register_script( 'jquery', get_template_directory_uri() . '/js/jquery-1.10.2.min.js'); 
-	wp_enqueue_script( 'jquery' ); 
+	wp_enqueue_style( 'default', get_template_directory_uri() . '/style.min.css', array(), '1.1.0' );
+	wp_deregister_script( 'jquery' );
+	wp_register_script( 'jquery', get_template_directory_uri() . '/js/jquery-1.10.2.min.js');
+	wp_enqueue_script( 'jquery' );
 
-	wp_enqueue_script( 'base', get_template_directory_uri() . '/js/global.js', array(), '1.1.0', true);
+	wp_enqueue_script( 'base', get_template_directory_uri() . '/js/global.min.js', array(), '1.1.0', true);
 
 	wp_localize_script('base', 'ajax', array(
 		'ajax_url' => admin_url('admin-ajax.php'),
