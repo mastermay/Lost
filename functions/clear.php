0c1bea61cbe80fbe5fa915d44e42ec618471e8c2
mastermay <986492616@qq.com>
2016-02-21 22:52:11 +0800

    remove JSON API in HTTP HEAD

--- functions/clear.php
+++ functions/clear.php
@@ -29,7 +29,7 @@ remove_action('wp_head','wlwmanifest_link');
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
 remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0 );
 
-add_filter('show_admin_bar','hide_admin_bar'); 
+add_filter('show_admin_bar','hide_admin_bar');
 function hide_admin_bar($flag) {
 	return false;
 }
@@ -57,17 +57,17 @@ function disable_emojis() {
     remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
     remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
     remove_action( 'wp_print_styles', 'print_emoji_styles' );
-    remove_action( 'admin_print_styles', 'print_emoji_styles' );    
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
     remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );  
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
     remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
     add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
 }
 add_action( 'init', 'disable_emojis' );
 /**
  * Filter function used to remove the tinymce emoji plugin.
- * 
- * @param    array  $plugins  
+ *
+ * @param    array  $plugins
  * @return   array  Difference betwen the two arrays
  */
 function disable_emojis_tinymce( $plugins ) {
@@ -130,6 +130,8 @@ if ( !get_option( 'use_smilies' ) )
 }
 smilies_reset();
 
+
+remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 );
 /*
  * remove WordPress RESTful API
  */
@@ -141,34 +143,34 @@ remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
 function disable_embeds_init() {
     /* @var WP $wp */
     global $wp;
- 
+
     // Remove the embed query var.
     $wp->public_query_vars = array_diff( $wp->public_query_vars, array(
         'embed',
     ) );
- 
+
     // Remove the REST API endpoint.
     remove_action( 'rest_api_init', 'wp_oembed_register_route' );
- 
+
     // Turn off
     add_filter( 'embed_oembed_discover', '__return_false' );
- 
+
     // Don't filter oEmbed results.
     remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
- 
+
     // Remove oEmbed discovery links.
     remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
- 
+
     // Remove oEmbed-specific JavaScript from the front-end and back-end.
     remove_action( 'wp_head', 'wp_oembed_add_host_js' );
     add_filter( 'tiny_mce_plugins', 'disable_embeds_tiny_mce_plugin' );
- 
+
     // Remove all embeds rewrite rules.
     add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
 }
- 
+
 add_action( 'init', 'disable_embeds_init', 9999 );
- 
+
 /**
  * Removes the 'wpembed' TinyMCE plugin.
  *
@@ -180,7 +182,7 @@ add_action( 'init', 'disable_embeds_init', 9999 );
 function disable_embeds_tiny_mce_plugin( $plugins ) {
     return array_diff( $plugins, array( 'wpembed' ) );
 }
- 
+
 /**
  * Remove all rewrite rules related to embeds.
  *
@@ -195,10 +197,10 @@ function disable_embeds_rewrites( $rules ) {
             unset( $rules[ $rule ] );
         }
     }
- 
+
     return $rules;
 }
- 
+
 /**
  * Remove embeds rewrite rules on plugin activation.
  *
@@ -208,9 +210,9 @@ function disable_embeds_remove_rewrite_rules() {
     add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
     flush_rewrite_rules();
 }
- 
+
 register_activation_hook( __FILE__, 'disable_embeds_remove_rewrite_rules' );
- 
+
 /**
  * Flush rewrite rules on plugin deactivation.
  *
@@ -220,7 +222,7 @@ function disable_embeds_flush_rewrite_rules() {
     remove_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
     flush_rewrite_rules();
 }
- 
+
 register_deactivation_hook( __FILE__, 'disable_embeds_flush_rewrite_rules' );
 
 ?>
